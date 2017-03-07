0ece3a17008cc5b90f33917d24e6c391fea815c3
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-03-07 15:18:21 -0800

    imported the router link

--- app/navBar.component.ts
+++ app/navBar.component.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
-
+import { RouterLink} from 'angular2/router'
 
 @Component({
     selector: 'navbar',
-    templateUrl: 'app/navbar.component.html'
+    templateUrl: 'app/navbar.component.html',
+    directives: [RouterLink]
 })
 
 export class NavBarComponent {
-    
+
 }
