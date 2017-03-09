d1fd5004f03c836c462a4413870d425b00b1b1c9
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-03-08 16:30:08 -0800

    added the js

--- app/users.component.js
+++ app/users.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './user.services'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,23 +10,33 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, user_services_1;
     var UsersComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (user_services_1_1) {
+                user_services_1 = user_services_1_1;
             }],
         execute: function() {
             UsersComponent = (function () {
-                function UsersComponent() {
+                function UsersComponent(_service) {
+                    this._service = _service;
                 }
+                UsersComponent.prototype.ngOnInit = function () {
+                    var _this = this;
+                    this._service.getUsers()
+                        .subscribe(function (users) { return _this.users = users; });
+                };
                 UsersComponent = __decorate([
                     core_1.Component({
                         selector: 'users',
-                        template: '<h1>Users</h1>'
+                        templateUrl: 'app/users.component.html',
+                        providers: [user_services_1.UserServices]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [user_services_1.UserServices])
                 ], UsersComponent);
                 return UsersComponent;
             }());
