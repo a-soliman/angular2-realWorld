51e692c173927cc30759232a214634fc0a675403
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-03-08 16:29:26 -0800

    updated the user compon

--- app/users.component.ts
+++ app/users.component.ts
@@ -1,11 +1,22 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import {UserServices} from './user.services';
 
 @Component({
     selector: 'users',
-    template: '<h1>Users</h1>'
+    templateUrl: 'app/users.component.html',
+    providers: [UserServices]
 })
 
 
-export class UsersComponent {
-    
+export class UsersComponent implements OnInit {
+    users: any[];
+
+    constructor(private _service: UserServices) {
+
+    }
+
+    ngOnInit() {
+        this._service.getUsers()
+            .subscribe(users => this.users = users);
+    }
 }
