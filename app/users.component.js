342e540900c70cb4efd481d5c75a4fbff78b3271
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-03-10 19:31:21 -0800

    users comp

--- app/users.component.js
+++ app/users.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './user.services'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', './user.services'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['angular2/core', './user.services'], function(exports_1, contex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, user_services_1;
+    var core_1, router_1, user_services_1;
     var UsersComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             },
             function (user_services_1_1) {
                 user_services_1 = user_services_1_1;
@@ -34,7 +37,8 @@ System.register(['angular2/core', './user.services'], function(exports_1, contex
                     core_1.Component({
                         selector: 'users',
                         templateUrl: 'app/users.component.html',
-                        providers: [user_services_1.UserServices]
+                        providers: [user_services_1.UserServices],
+                        directives: [router_1.RouterLink]
                     }), 
                     __metadata('design:paramtypes', [user_services_1.UserServices])
                 ], UsersComponent);
