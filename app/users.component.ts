4a22a643267a9584caf8bba8141d71b8aca45f4f
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-03-10 19:30:05 -0800

    added the Router-link

--- app/users.component.ts
+++ app/users.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from 'angular2/core';
+import { RouterLink } from 'angular2/router';
+
 import {UserServices} from './user.services';
+
 
 @Component({
     selector: 'users',
     templateUrl: 'app/users.component.html',
-    providers: [UserServices]
+    providers: [UserServices],
+    directives: [RouterLink]
 })
 
 
