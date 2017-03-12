0e86cd8f5c85bfc6ba406cd558d65782fb5f44e1
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-03-11 19:32:29 -0800

    coded the new path for users/new

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,12 +4,15 @@ import { RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
 import {NavBarComponent} from './navbar.component';
 import { HomeComponent } from './home.component';
 import { UsersComponent } from './users.component';
+import {NewUserComponent} from './newuser.component';
 import {PostsComponent} from './posts.component'
+
 
 @RouteConfig([
     { path: '/', name: 'Home', component: HomeComponent, useAsDefault: true},
     { path: '/users' ,name:'Users', component: UsersComponent},
     { path:'/posts', name: 'Posts', component: PostsComponent},
+    { path: '/users/new', name: 'NewUser', component: NewUserComponent},
     { path: '/*other', name: 'Other', redirectTo: ['Home']}
 ])
 
